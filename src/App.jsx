ffba2c3e39ef92cfbf5bc41d7bf0bd2d3faf3e38
felipinhobgtom <felipinhobgouveia@gmail.com>
2024-10-11 18:41:44 -0300

    done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import HelloWorldProps from './components/HelloWorldProps'
+
+function App() {
+
+  return (
+    <HelloWorldProps HelloWorld='Olá mundo, com props!'/>
+  )
+}
+
+export default App
---
+++ src/components/HelloWorldProps.jsx
@@ -0,0 +1,8 @@
+function HelloWorldProps(props){
+
+    return(
+        <h1>{props.HelloWorld}</h1>
+    )
+}
+
+export default HelloWorldProps
